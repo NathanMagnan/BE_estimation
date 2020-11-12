7e585231fd8bd7a5516e87a3f85bcdecd48cc4b4
NathanMagnan <nathan.magnan@polytechnique.edu>
2020-11-12 13:25:27 +0100

    Metropolis_hastings corrected

--- V_test_metropolis_hastings.m
+++ V_test_metropolis_hastings.m
@@ -1,10 +1,10 @@
 %% choosing the metropolis-hastings parameters
-n_steps = 100000;
+n_steps = 1000;
 n_burns = 1; %% Should not be 0
 tau = 0.0105;
 
 %% Running the metropolis-hastings algorithm
-chain = V_Metropolis_Hastings(n_steps, n_burns, tau);
+[chain, rate_accept] = V_Metropolis_Hastings(n_steps, n_burns, tau);
 s = size(chain);
 
 %% plotting the chain (parameter a)
@@ -15,6 +15,7 @@ for k = 1:(s(2))
     X(k) = k;
 end
 
-Y = chain(6, 1:s(2));
+Y = chain(1, 1:s(2));
 
 plot(X, Y)
+rate_accept
